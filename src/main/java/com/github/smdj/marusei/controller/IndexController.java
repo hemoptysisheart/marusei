73c94dd418dc04908bd922bed07f6e3299480437
hellosamuel <samuelis419@gmail.com>
2019-01-06 15:40:19 +0900

    ADD SignUp 유효성 검사 및 기능 추가

--- src/main/java/com/github/smdj/marusei/controller/IndexController.java
+++ src/main/java/com/github/smdj/marusei/controller/IndexController.java
@@ -1,11 +1,24 @@
 package com.github.smdj.marusei.controller;
 
+import com.github.smdj.marusei.controller.request.SignUpRequest;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 
 @RequestMapping
 public interface IndexController {
     @GetMapping("")
     String index();
+
+    @GetMapping("/signup")
+    String signUpForm(Model model);
+
+    @PostMapping("/signup")
+    String signUp(@ModelAttribute @Valid SignUpRequest signUpRequest, BindingResult bindingResult, Model model);
 }
